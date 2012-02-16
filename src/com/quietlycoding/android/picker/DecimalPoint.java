176ea28ef762c4080f10a9d7ffcb5656ea8f74f5
Wim Rijnders <info@axizo.nl>
2012-02-16 12:06:22 +0100

    Added files for Decimal Point activity.

---
+++ src/com/quietlycoding/android/picker/DecimalPoint.java
@@ -0,0 +1,16 @@
+package com.quietlycoding.android.picker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class DecimalPoint extends Activity  {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.decimal_point);
+    }
+}
